924edb3664e4d91e754ec999f80e484d598e3f1d
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-06-18 21:05:17 +0900

    Add Weather

---
+++ js/box1.js
@@ -0,0 +1,50 @@
+//box1에 포함되어 있는 날짜, 시간, 명언을 표시합니다.
+
+const date = document.querySelector("#date");  //날짜
+const time = document.querySelector("#time");  //시간
+const poem = document.querySelector("#poem");
+const poemDetail = document.querySelector("#poemDetail");
+
+const poems = [
+    {poem: "어제의 길이 오늘 지도에서 사라져도 그대는 항해를 시작한다. 길 없는 바다가 그대의 대지이기에.",
+     detail:"-「淑女에게」, 최시한"},
+    {poem: "바다는 비에 젖지 않는다.",
+    detail:"-「노인과 바다」, 헤밍웨이"},
+    {poem: "푸른 바다에는 고래가 있어야지. 고래 한 마리 키우지 않으면 청년이 아니지",
+    detail:"-「고래를 위하여」, 정호승"},
+    {poem: "오늘은 오랜만에 내가 나에게 푸른 엽서를 쓴다 어서 일어나 섬들이 많은 바다로 가자고",
+    detail:"-「내가 나에게」, 이해인"},
+    {poem: "누구나 자기만의 바닷가가 하나씩 있으면 좋다 자기만의 바닷가로 달려가 쓰러지는 게 좋다",
+    detail:"-「바닷가에 대하여」, 정호승"}
+];
+
+//날짜 표시
+function handleDate(){
+    const today = new Date();
+    const month = String(today.getMonth()+1).padStart(2,"0");
+    const day = String(today.getDate()).padStart(2,"0");
+    date.innerText = `${month}/${day}`
+}
+
+//시간 표시
+function handleTime(){
+    const today = new Date();
+    const hour = String(today.getHours()).padStart(2,"0");
+    const minutes = String(today.getMinutes()).padStart(2,"0");
+    time.innerText = `${hour}:${minutes}`;
+}
+
+//시 구절 표시
+function handlePoem(){
+    const chosenPoem = poems[Math.floor(Math.random() * poems.length)];
+    poem.innerText = chosenPoem.poem;
+    poemDetail.innerText = chosenPoem.detail;
+}
+
+
+handleDate();
+handleTime();
+handlePoem();
+setInterval(handleDate,1000);
+setInterval(handleTime,1000);
+
---
+++ js/weather.js
@@ -0,0 +1,40 @@
+//box2 날씨 박스 부분
+const rocate = document.querySelector("#rocate");
+const weatherIcon = document.querySelector("#weatherIcon");
+const nowtemp = document.querySelector("#nowtemp");
+const maxmin_temp = document.querySelector("#maxmin_temp");
+
+const API_KEY = "7936a67affe0094bce8595b1779afbee";
+
+
+function onGeoSuccess(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const city = data.name;
+        const country = data.sys.country;
+        const weather = data.weather[0].main;
+        const temp = data.main.temp;
+        const min_temp = Math.ceil(data.main.temp_min);
+        const max_temp = Math.ceil(data.main.temp_max);
+        const selectIcon = data.weather[0].icon;
+        const iconsrc = `img/icons/${selectIcon}.png`;
+
+        rocate.innerText = `${country}/${city}`;
+        weatherIcon.setAttribute('src', iconsrc);
+        nowtemp.innerText=`${temp}°`;
+        maxmin_temp.innerText = `최고 ${max_temp}°/최저 ${min_temp}°`;
+
+
+    });
+
+}
+
+
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess,onGeoError);
